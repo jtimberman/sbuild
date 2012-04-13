5f1f29adf48c430df1f1d295433266c31ce05a35
Andrew Crump <andrew@kotirisoftware.com>
2012-04-13 21:09:09 +0100

    Increment version for development.

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "cookbooks@opscode.com"
 license          "Apache 2.0"
 description      "Creates an sbuild host for debian packages."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.3.3"
+version          "0.3.5"
 
 depends "xfs"
 depends "lvm"
